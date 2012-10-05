af8bb67a902213dcf7239f10573cb16d917be210
Mario Kadastik <mario@cern.ch>
2012-10-05 16:45:51 +0300

    Implemented trigger based filtering before analysis

--- SingleTopAnalysis/singletopanalysis_cfg.py
+++ SingleTopAnalysis/singletopanalysis_cfg.py
@@ -4,19 +4,25 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string('out.root')
 )
 
+process.load('AnalysisCode.TriggerFilter.triggerfilter_cfi')
+
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:/hdfs/local/mario/singletop/53sync/tbar_tuple.root'
+        'file:/hdfs/local/mario/singletop/53sync/tbar_tuple_small.root'
     )
 )
 
 process.load('AnalysisCode.SingleTopAnalysis.singletopanalysis_cfi')
+#process.trig.debug=cms.bool(True)
 
-process.p = cms.Path(process.stanal)
+process.p = cms.Path(
+    process.trig+
+    process.stanal
+)
