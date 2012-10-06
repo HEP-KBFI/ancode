87d596a466560175ba8d0d7c14f7e29eb3a45323
Mario Kadastik <mario@cern.ch>
2012-10-06 11:04:45 +0300

    Running on the full file for sync, without trigger

--- SingleTopAnalysis/singletopanalysis_cfg.py
+++ SingleTopAnalysis/singletopanalysis_cfg.py
@@ -4,7 +4,7 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string('out.root')
@@ -15,7 +15,7 @@ process.load('AnalysisCode.TriggerFilter.triggerfilter_cfi')
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:/hdfs/local/mario/singletop/53sync/tbar_tuple_small.root'
+        'file:/hdfs/local/mario/singletop/53sync/tbar_tuple.root'
     )
 )
 
@@ -23,6 +23,6 @@ process.load('AnalysisCode.SingleTopAnalysis.singletopanalysis_cfi')
 #process.trig.debug=cms.bool(True)
 
 process.p = cms.Path(
-    process.trig+
+    #process.trig+
     process.stanal
 )
