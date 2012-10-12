c5e055c6665306d34814c877e66408c4bdc66f39
Mario Kadastik <mario@cern.ch>
2012-10-12 12:53:19 +0300

    Switched to new step1 output with new corrections

--- SingleTopAnalysis/singletopanalysis_cfg.py
+++ SingleTopAnalysis/singletopanalysis_cfg.py
@@ -15,7 +15,7 @@ process.load('AnalysisCode.TriggerFilter.triggerfilter_cfi')
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:/hdfs/local/mario/singletop/53sync/tbar_tuple.root'
+        'file:/hdfs/local/mario/singletop/53sync/tbar_tuple_sync2.root'
     )
 )
 process.load('AnalysisCode.SingleTopAnalysis.singletopanalysis_cfi')
