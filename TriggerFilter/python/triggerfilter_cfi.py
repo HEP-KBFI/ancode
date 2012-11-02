74ad90d56cce5454932f50f9e511e404cabf37e7
Mario Kadastik <mario@cern.ch>
2012-11-02 16:28:38 +0200

    Triggers updated to include all combinations

--- TriggerFilter/python/triggerfilter_cfi.py
+++ TriggerFilter/python/triggerfilter_cfi.py
@@ -1,9 +1,55 @@
 import FWCore.ParameterSet.Config as cms
 
-trig = cms.EDFilter('TriggerFilter',
+trigMuAll = cms.EDFilter('TriggerFilter',
+	trigLabel	= cms.InputTag("patTriggerEvent"),
+	yesTrig		= cms.vstring( 	"HLT_IsoMu24_eta2p1_v", "HLT_IsoMu17_eta2p1_CentralPFNoPUJet30_BTagIPIter_v" ),
+	noTrig		= cms.vstring(),
+	debug		= cms.bool(False)
+)
+
+trigMuBtag = cms.EDFilter('TriggerFilter',
+	trigLabel	= cms.InputTag("patTriggerEvent"),
+	yesTrig		= cms.vstring( 	"HLT_IsoMu17_eta2p1_CentralPFNoPUJet30_BTagIPIter_v" ),
+	noTrig		= cms.vstring(),
+	debug		= cms.bool(False)
+)
+
+trigMu = cms.EDFilter('TriggerFilter',
 	trigLabel	= cms.InputTag("patTriggerEvent"),
 	yesTrig		= cms.vstring( 	"HLT_IsoMu24_eta2p1_v" ),
 	noTrig		= cms.vstring(),
 	debug		= cms.bool(False)
 )
 
+trigEleAll = cms.EDFilter('TriggerFilter',
+	trigLabel	= cms.InputTag("patTriggerEvent"),
+	yesTrig		= cms.vstring( 	"HLT_Ele27_WP80_v", "HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralPFNoPUJet30_BTagIPIter_v" ),
+	noTrig		= cms.vstring(),
+	debug		= cms.bool(False)
+)
+
+trigEleBtag = cms.EDFilter('TriggerFilter',
+	trigLabel	= cms.InputTag("patTriggerEvent"),
+	yesTrig		= cms.vstring( 	"HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralPFNoPUJet30_BTagIPIter_v" ),
+	noTrig		= cms.vstring(),
+	debug		= cms.bool(False)
+)
+
+trigEle = cms.EDFilter('TriggerFilter',
+	trigLabel	= cms.InputTag("patTriggerEvent"),
+	yesTrig		= cms.vstring( 	"HLT_Ele27_WP80_v" ),
+	noTrig		= cms.vstring(),
+	debug		= cms.bool(False)
+)
+
+trig = cms.EDFilter('TriggerFilter',
+    trigLabel   = cms.InputTag("patTriggerEvent"),
+    yesTrig     = cms.vstring(  
+        "HLT_Ele27_WP80_v", 
+        "HLT_Ele25_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_CentralPFNoPUJet30_BTagIPIter_v", 
+        "HLT_IsoMu24_eta2p1_v", 
+        "HLT_IsoMu17_eta2p1_CentralPFNoPUJet30_BTagIPIter_v"
+    ),
+    noTrig      = cms.vstring(),
+    debug       = cms.bool(False)
+)
