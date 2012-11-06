188a1eb4495521c314d6bcfb77cbfe3638ddc4d9
Liis Rebane <polaarrebane@gmail.com>
2012-11-06 04:07:38 +0200

    add p4 of reconstructed W

--- AnalysisDataFormats/interface/TopJetPair.h
+++ AnalysisDataFormats/interface/TopJetPair.h
@@ -15,8 +15,8 @@ class TopJetPair //: public reco::LeafCandidate
   /// destructor
   ~TopJetPair() {}
   
-  double mTW() const { return mTW_; }
   double costh() const { return costh_; }
+  const reco::Candidate::LorentzVector& p4W() const { return p4W_; }
   const reco::Candidate::LorentzVector& p4nu() const { return p4nu_; }
   const reco::Candidate::LorentzVector& p4top() const { return p4top_; }
   const reco::Candidate::LorentzVector& p4ljetTopCM() const { return p4ljetTopCM_; }
@@ -26,7 +26,7 @@ class TopJetPair //: public reco::LeafCandidate
 
   /// allow only TopAlgorithm to change values of data-members
    friend class TopJetPairAlgorithm;
-   void setmTW(double mTW) { mTW_ = mTW; }
+   void setp4W(const reco::Candidate::LorentzVector& p4W) { p4W_ = p4W; }
    void setcosth(double costh) { costh_ = costh; }
    void setp4nu(const reco::Candidate::LorentzVector& p4nu) { p4nu_ = p4nu; }
    void setp4top(const reco::Candidate::LorentzVector& p4top) { p4top_ = p4top; }
@@ -35,8 +35,8 @@ class TopJetPair //: public reco::LeafCandidate
 
 
   /// tau 4-vector
-   double mTW_;
    double costh_;
+   reco::Candidate::LorentzVector p4W_; 
    reco::Candidate::LorentzVector p4nu_;
    reco::Candidate::LorentzVector p4top_;
    reco::Candidate::LorentzVector p4ljetTopCM_;
